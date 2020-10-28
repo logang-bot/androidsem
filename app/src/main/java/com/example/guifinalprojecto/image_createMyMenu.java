12c164bfb11babf5fb19c48df1febbef7abfeb46
Erika <erikanoemychuya@gmail.com>
2020-10-28 18:24:04 -0400

    layout y class do it create menu

--- app/src/main/java/com/example/guifinalprojecto/image_createMyMenu.java
+++ app/src/main/java/com/example/guifinalprojecto/image_createMyMenu.java
@@ -2,13 +2,43 @@ package com.example.guifinalprojecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class image_createMyMenu extends AppCompatActivity {
+    private String NombreRest;
+    private Button siguiente;
+    private image_createMyMenu root=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_create_my_menu);
+
+        Bundle bundle = this.getIntent().getExtras();
+        NombreRest= bundle.getString("nombreR");
+        Toast.makeText(this," idRest = "+NombreRest.toString(), Toast.LENGTH_LONG).show();
+
+        TextView NombreRestaurant=findViewById(R.id.mostrarnombreRest);
+        NombreRestaurant.setText(NombreRest);
+
+
+        siguiente= this.findViewById(R.id.new_SigT);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(root, listMyMenus.class);
+                intent.putExtra("nombreR", NombreRest);
+                //intent.putExtra("logoRes",logoR);
+                Toast.makeText(getApplicationContext(),"Producto creado",Toast.LENGTH_LONG).show();
+                root.startActivity(intent);
+            }
+        });
+
     }
 }
